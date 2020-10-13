98d83ae0727aa71ca7c8120cd9ed3c38a16e70ad
mjankovskij <70883106+mjankovskij@users.noreply.github.com>
2020-10-13 11:46:12 +0300

    more adaptive

--- js/header.js
+++ js/header.js
@@ -1,8 +1,17 @@
 // scrollinimas iki nustatyto aukscio
-function scrollUp(x) {
-    window.scrollTo(0, x);
+function scrollUp(whichItem) {
+    let totalHeightMenu = -120;
+    //for mobile scrollHeight
+    if (window.innerWidth <= 800) { totalHeightMenu -= 520; }
+    // check scrollHeight everytime on scroll, because can be resized
+    for (let i = 0; i <= whichItem; i++) {
+        totalHeightMenu += document.querySelectorAll(".container")[i].scrollHeight;
+    }
+    // scroll to choosen height
+    window.scrollTo(0, totalHeightMenu);
 }
-// reikia perkelt i data
+
+// Data ateityje perkelsiu i date.js ar kita...
 const menuDate = [
     { menuTitle: 'Home' },
     { menuTitle: 'About' },
@@ -14,14 +23,13 @@ const menuDate = [
 ]
 
 // prideda meniu i 
-let totalHeightMenu = -120;
 window.onload = function() {
     for (let i = 0; i < menuDate.length; i++) {
         let nameThisMenu = menuDate[i].menuTitle;
-        totalHeightMenu += document.querySelectorAll(".container")[i].scrollHeight;
-        document.querySelector("nav p").innerHTML += `<p onclick=scrollUp(${totalHeightMenu})>${nameThisMenu}</p>`;
+        document.querySelector("nav").innerHTML += `<p onclick=scrollUp(${i})>${nameThisMenu}</p>`;
     }
-    // document.querySelectorAll("nav p p")[1].style.borderBottom = "solid 2px #ffffff";
+    // uzkraunant Home pabraukiamas
+    document.querySelectorAll("nav p")[0].style.borderBottom = "solid 2px #fff";
     textPrint();
 }
 
@@ -29,53 +37,63 @@ window.onscroll = function() {
     // per mobile menu, jei pasirenka, auto uzdaro ji
     document.querySelector("nav").style.height = `0px`;
     dropdowns = 0;
-
     // jei nenuscrolinta
     if (document.documentElement.scrollTop > 620) {
+        // header uzrasai virsun
         var x = document.querySelector("header").style;
         x.paddingBottom = "10px";
         x.paddingTop = "30px";
+
+        //header nuscrolinus fixed virsuj
         var z = document.getElementsByClassName("container")[0].style;
         z.backgroundColor = "#ffffff";
         z.position = "fixed";
         z.boxShadow = "0 1px 50px 5px #e7e6e63b";
-
+        // link (LOGO) color change on scrolled
         var y = document.querySelector(".container>header a").style;
         y.color = "#000000";
 
+        // menu uzrasu spalvos keitimas
         let lengthLinks = document.querySelectorAll(".container>header p").length;
         for (let i = 0; i < lengthLinks; i++) {
             document.querySelectorAll(".container>header p")[i].style.color = "#000000";
         }
 
+        // papildomi pakeitimai
         document.getElementById("particles-js").style.height = "690px";
         document.querySelector(".container>header>.menuChoices").style.marginTop = "-10px";
         document.getElementsByClassName("menuChoices")[0].style.color = "#000";
 
     } else {
         // jei nenuscrolitna, virsus
-        document.getElementById("particles-js").style.height = "752px";
-        document.querySelector(".container>header>.menuChoices").style.marginTop = "0px";
-        document.getElementsByClassName("menuChoices")[0].style.color = "#fff";
 
+        // header uzrasai apacion
         var x = document.querySelector("header").style;
         x.paddingTop = "40px";
         x.paddingBottom = "0px";
 
+        //header grizus i virsu pradingsta is fixed pozicijos
         var z = document.getElementsByClassName("container")[0].style;
         z.backgroundColor = null;
         z.position = "relative";
         z.boxShadow = null;
 
+        // link (LOGO) color change on scrolledUp
         var y = document.querySelector(".container>header a").style;
         y.color = "#ffffff";
 
+        // menu uzrasu spalvos keitimas
         if (window.innerWidth >= 800) {
             let lengthLinks = document.querySelectorAll(".container>header p").length;
             for (let i = 0; i < lengthLinks; i++) {
                 document.querySelectorAll(".container>header p")[i].style.color = "#ffffff";
             }
         }
+
+        // papildomi pakeitimai
+        document.getElementById("particles-js").style.height = "752px";
+        document.querySelector(".container>header>.menuChoices").style.marginTop = "0px";
+        document.getElementsByClassName("menuChoices")[0].style.color = "#fff";
     }
     // Reikia pridet pabraukimus po pasirinkta meniu dalim !!!!!!!!!!!!
     // pabraukimas apacioj
@@ -85,24 +103,25 @@ window.onscroll = function() {
         document.querySelectorAll(".container>header p")[i].style.borderBottom = null;
     }
 
-    // Reikia pridet pabraukimus po pasirinkta meniu dalim !!!!!!!!!!!!
-
-    // if (document.body.scrollTop >= heightContact || document.documentElement.scrollTop >= heightContact) {
-    //     document.querySelectorAll(".container>header p")[6].style.borderBottom = "solid 2px #000000";
-    // } else if (document.body.scrollTop >= heightBlog || document.documentElement.scrollTop >= heightBlog) {
-    //     document.querySelectorAll(".container>header p")[5].style.borderBottom = "solid 2px #000000";
-    // } else if (document.body.scrollTop >= heightClients || document.documentElement.scrollTop >= heightClients) {
-    //     document.querySelectorAll(".container>header p")[4].style.borderBottom = "solid 2px #000000";
-    // } else if (document.body.scrollTop >= heightWorks || document.documentElement.scrollTop >= heightWorks) {
-    //     document.querySelectorAll(".container>header p")[3].style.borderBottom = "solid 2px #000000";
-    // } else if (document.body.scrollTop >= heightServices || document.documentElement.scrollTop >= heightServices) {
-    //     document.querySelectorAll(".container>header p")[2].style.borderBottom = "solid 2px #000000";
-    // } else if (document.body.scrollTop >= heightAbout || document.documentElement.scrollTop >= heightAbout) {
-    //     document.querySelectorAll(".container>header p")[1].style.borderBottom = "solid 2px #000000";
-    // } else if (document.body.scrollTop < heightAbout || document.documentElement.scrollTop < heightAbout) {
-    //     document.querySelectorAll(".container>header p")[0].style.borderBottom = "solid 2px #ffffff";
-    // }
-};
+    // pabraukimas apacioj menu
+    if (window.innerWidth > 800) {
+        let totalHeightMenu = -200;
+        for (let i = 0; i <= menuDate.length; i++) {
+            totalHeightMenu += document.querySelectorAll(".container")[i].scrollHeight;
+            if (i > 0) {
+                if (document.body.scrollTop >= totalHeightMenu || document.documentElement.scrollTop >= totalHeightMenu) {
+                    document.querySelectorAll("nav p")[i].style.borderBottom = "solid 2px #000";
+                    let x = i - 1;
+                    if (x > -1) {
+                        document.querySelectorAll("nav p")[x].style.borderBottom = "solid 2px #fff";
+                    }
+                }
+            } else {
+                document.querySelectorAll("nav p")[0].style.borderBottom = "solid 2px #fff";
+            }
+        }
+    }
+}
 
 
 // teksto spausdinimas visu triju besikeicianciu
@@ -162,14 +181,18 @@ document.getElementsByClassName("menuChoices")[0].addEventListener("click", func
 // dropdown papildymas
 window.onresize = function() {
     if (window.innerWidth > 800) {
-        let lengthLinks = document.querySelectorAll(".container>header p").length;
-        for (let i = 0; i < lengthLinks; i++) {
-            document.querySelectorAll(".container>header p")[i].style.color = "#ffffff";
+        if (document.documentElement.scrollTop < 620) {
+            let lengthLinks = document.querySelectorAll(".container>header p").length;
+            for (let i = 0; i < lengthLinks; i++) {
+                document.querySelectorAll(".container>header p")[i].style.color = "#ffffff";
+            }
         }
     } else {
         let lengthLinks = document.querySelectorAll(".container>header p").length;
         for (let i = 0; i < lengthLinks; i++) {
             document.querySelectorAll(".container>header p")[i].style.color = "#000000";
+            document.querySelectorAll("nav p")[i].style.borderBottom = "solid 2px #fff";
         }
     }
+
 }
