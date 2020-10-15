545d9205202998ced1ab40978eb4342122d9e0d1
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-10-15 13:43:52 +0300

    removed clients z index error

--- js/clients.js
+++ js/clients.js
@@ -1,23 +1,25 @@
 const clientNumbers = document.querySelectorAll('.counter');
 const speed = 600;
-clientNumbers.forEach(counter =>{
+clientNumbers.forEach(counter => {
     const updateCount = () => {
         const target = +counter.getAttribute('data-target');
         const count = +counter.innerText;
-        
+
         const loadSpeed = target / speed;
-        
-        if (count<target && target!==9450) {
-            counter.innerText = Math.ceil(count+loadSpeed);
+
+        if (count < target && target !== 9450) {
+            counter.innerText = Math.ceil(count + loadSpeed);
             setTimeout(updateCount, 1)
-        } if (count<target && target ==9450) {
-            counter.innerText = Math.floor(count+loadSpeed);
+        } if (count < target && target == 9450) {
+            counter.innerText = Math.floor(count + loadSpeed);
             setTimeout(updateCount, 1)
         }
-         else {
+        else {
             count.innerText = target;
         }
 
     }
     updateCount();
 });
+
+export default clients;
