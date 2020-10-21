9ef6ecdad4895e55ccb2682395d30c0167210f4c
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-10-21 22:43:19 +0300

    styling clients...

--- js/components/clients/renderSingleClients.js
+++ js/components/clients/renderSingleClients.js
@@ -1,12 +1,13 @@
 function renderSingleClients(data) {
 
-    return `<div class="clientSlide one>
+    return `<div class="clientSlide ${data.positionNumber}>
                 <div class="alex">
-                    <img id='alex' src="${data.photo}" alt="Alex-photo">
+                    <img class="alex" id='alex' src="${data.photo}" alt="Alex-photo">
                 </div>
                 <p>${data.text3}</p>
                 <h3>${data.name1}</h3>
                 <p>${data.name2}</p>
+                <br>
             </div>`;
 }
 // data-index="${middleIndex}
--- js/data/clientsData.js
+++ js/data/clientsData.js
@@ -3,20 +3,23 @@ const clientsData = [
         photo: './img/alex.jpg',
         text3: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, neque repellendus,illumdistinctio necessitatibus dignissimos cum. Consectetur nemo quam modi rationeconsequatur!',
         name1: 'Alex Smith',
-        name2: 'Envanto Customer'
+        name2: 'Envanto Customer',
+        positionNumber: 'clientOne'
     },
     {
         photo: './img/alex.jpg',
         text3: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, neque repellendus,illumdistinctio necessitatibus dignissimos cum. Consectetur nemo quam modi rationeconsequatur!',
         name1: 'Alex Smith',
-        name2: 'Envanto Customer'
+        name2: 'Envanto Customer',
+        positionNumber: 'clientTwo'
     },
     {
         photo: './img/alex.jpg',
         text3: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, neque repellendus,illumdistinctio necessitatibus dignissimos cum. Consectetur nemo quam modi rationeconsequatur!',
         name1: 'Alex Smith',
-        name2: 'Envanto Customer'
+        name2: 'Envanto Customer',
+        positionNumber: 'clientThree'
     }
 ]
 
-export { clientsData }
+export { clientsData };
