6f0c5d92f191fdbba7bc6b66dc468f22f03db78a
qReilas <Edvardas.nugaris@gmail.com>
2020-10-22 14:18:17 +0300

    update css

--- js/components/works/isWorksDataValid.js
+++ js/components/works/isWorksDataValid.js
@@ -1,21 +1,23 @@
-function isWorksDataValid(data) {
+import portfolio from "../../data/worksdata";
+
+function isWorksDataValid(portfolio) {
     let errors = [];
 
-    if (typeof data !== 'object' || data === null) {
+    if (typeof portfolio !== 'object' || portfolio === null) {
         console.log('data must be object.');
         return false;
     }
-    if (data.length < 1 || Object.keys(data).length < 1) {
+    if (portfolio.length < 1 || Object.keys(portfolio).length < 1) {
         errors.push('here is no data.');
     }
-    if (typeof data.works.title !== 'string') {
+    if (typeof portfolio.works.title !== 'string') {
         console.log('menu title must be string.');
         return false;
     }
-    if (data.works.title.length < 2 || data.works.title.length > 15) {
+    if (portfolio.works.title.length < 2 || portfolio.works.title.length > 15) {
         errors.push('menu title length must be 2-15 symbols.');
     }
-    if (!/^[a-zA-Z]+$/.test(data.works.title)) {
+    if (!/^[a-zA-Z]+$/.test(portfolio.works.title)) {
         errors.push('Menu title must contains only letters.');
     }
 
